87e3809ec93eb27c8d56afc80fd7a72b870e930d
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2017-08-20 01:55:51 +0530

    udpate

--- 3_ML_with_python_regression_features_and_labels/3_ml_regression_features_and_labels.py
+++ 3_ML_with_python_regression_features_and_labels/3_ml_regression_features_and_labels.py
@@ -3,6 +3,13 @@
 
 import pandas as pd
 import quandl
+import sys
+sys.path.append('../authentication/')
+import auth
+import math
+
+# Authenticate with quandl
+auth.authenticat_with_quandl()
 
 # Get the data from quandl 
 df = quandl.get('WIKI/GOOGL')
@@ -26,7 +33,7 @@ df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.
 # define a new dataframe / filter only required columns
 df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 
-#print(df)
+#print(df.head())
 # Here is Adj. Close column a feture or a Label ?
 # Firstly what is difference b/w Feature and a Label
 # Feature: 	A column of Data in input set
@@ -35,3 +42,18 @@ df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 
 # In our case 'Adj. Close' is a feature
 
+# Creating a Forecast column
+forecast_col = 'Adj. Close'
+
+# Fill empty entries with user defined meaningful value
+# You can't work NAN data so need to replace the missing data
+df.fillna(-9999, inplace=True)
+
+# Predict out of 1% of database hence 0.01 factor is taken
+Factor = 0.01
+forecast_out = int(math.ceil(Factor * len(df)))
+
+df['label'] = df[forecast_col].shift(-forecast_out)
+df.dropna(inplace=True)
+print(df.head())
+print(df.tail())
